396c880080b11bbae02eba713fffacf2b48af270
John Luis Magtoto <100548091+JohnLuis07@users.noreply.github.com>
2022-03-03 19:03:13 +0800

    Add files via upload

---
+++ CMSC 21/Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int difference;
+
+    difference = 15 - 87;
+    printf("The difference of 15 and 87 is %d", difference);
+
+    return 0;
+}
---
+++ CMSC 21/Lecture1/Assignments/assignment_lec4_c4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//The void there before has a capital letter at the beginning so I've change it
+int main(void)
+//Theres no opening bracket so I've put it one.
+{
+    //int before was all caps so I've change it all in lowercase
+    int sum;
+    //The comment below doesn't have '*/' or the first symbol doesn't look like this '//' so I've change it
+    // COMPUTE RESULT
+
+    //Before the line before doesn't have a semicolon after it
+    sum = 25 + 37 - 19;
+
+    //The comment below before doesn't have the correct format
+    /* DISPLAY RESULTS */
+
+    //There's no comma after the quotation before
+    printf("The answer is %i\n", sum);
+    return 0;
+}
